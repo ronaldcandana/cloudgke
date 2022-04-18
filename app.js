ec225366f46a2585f05e71037d35d03337bb768c
ronaldcandana <103921448+ronaldcandana@users.noreply.github.com>
2022-04-18 16:45:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('ronald candana, 52019032, teknik informatika!\n')
 })
 
 app.listen(PORT, HOST)
-console.log(`Running on http://${HOST}:${PORT}`)
+console.log(`Running on http://${HOST}:${PORT}')
